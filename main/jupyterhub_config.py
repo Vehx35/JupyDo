eb93ea767a4181e1711131cfa6fb53eb33e48bae
Francesco Maria Antonio Micocci <44471116+Vehx35@users.noreply.github.com>
2025-04-02 12:10:33 +0200

    messing around
    some things to fix some not to

--- main/jupyterhub_config.py
+++ main/jupyterhub_config.py
@@ -1,10 +1,14 @@
 # Configuration file for JupyterHub
-
+import os, nativeauthenticator
+c.JupyterHub.template_paths = [f"{os.path.dirname(nativeauthenticator.__file__)}/templates/"]
 c = get_config()  # noqa: F821
 
 # Basic JupyterHub configuration
 c.JupyterHub.bind_url = 'http://:8000'
 c.JupyterHub.hub_ip = '0.0.0.0'
+
+# Possibility to enable named servers
+#c.JupyterHub.allow_named_servers = True
 
 # Authenticate users with Native Authenticator
 c.JupyterHub.authenticator_class = 'nativeauthenticator.NativeAuthenticator'
@@ -13,8 +17,8 @@ c.JupyterHub.authenticator_class = 'nativeauthenticator.NativeAuthenticator'
 c.NativeAuthenticator.open_signup = False
 
 # Allowed admins
-admin = 'admin_user'  # Replace with your admin username
-c.Authenticator.admin_users = {admin}
+admin = 'limo'  # Replace with your admin username
+c.Authenticator.admin_users = {admin} # For some reason, admin user is not being allowed by default
 
 # Allow all signed-up users to login
 c.Authenticator.allow_all = True
