0c0e1ac07884ec63f5cf1de4756337bee1cb185c
Francesco Maria Antonio Micocci <44471116+Vehx35@users.noreply.github.com>
2025-04-02 15:05:16 +0200

    Update jupyterhub_config.py
    somehow i made it work

--- main/jupyterhub_config.py
+++ main/jupyterhub_config.py
@@ -20,6 +20,16 @@ c.NativeAuthenticator.open_signup = False
 c.JupyterHub.spawner_class = DockerSpawner
 c.NativeAuthenticator.create_system_users = True
 
+#trying to make it work
+from jupyter_client.localinterfaces import public_ips
+
+c.JupyterHub.hub_ip = public_ips()[0]
+
+c.JupyterHub.log_level = 'DEBUG'
+
+c.DockerSpawner.network_name = 'jupyterhub_network'
+c.JupyterHub.hub_connect_ip = 'jupyterhub'  # Use the container name of the JupyterHub instance
+
 
 notebook_dir = '/home/jovyan/work'
 c.DockerSpawner.notebook_dir = notebook_dir
